2d898ee414ab09327849ac557d24374f682685c3
rolandlee0308 <rolandlee500@gmail.com>
2022-05-22 08:32:48 -0700

    adding frontend folder and implementing register and login pages

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  const onChange = (e: any) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div>
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
